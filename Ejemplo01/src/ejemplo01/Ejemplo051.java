7878bad8dea7c3e42a89bfaa1113a7b2cebe3746
CesarM3-A <cesarmateoab2007@gmail.com>
2025-10-29 12:17:48 -0500

    Ejemplo051

---
+++ Ejemplo01/src/ejemplo01/Ejemplo051.java
@@ -0,0 +1,34 @@
+/*
+René Rolando
+
+Elizalde Solano
+
+1983
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo051 {
+    public static void main(String[] args) {
+        // Secuencia de escape \n y \t
+        String nombreEstudiante = "René Rolando";
+        String apellidoEstudiante = "Elizalde Solano";
+        int nacimiento = 1983;
+        
+        System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
+                nacimiento);
+        
+        System.out.println(nombreEstudiante+"\n\n"+apellidoEstudiante+ "\n\n"+ 
+                nacimiento);
+        
+        System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
+                "\n"+ nacimiento);
+        //System.out.println(nombreEstudiante+"\t"+apellidoEstudiante); 
+       // el "\t" es un tabulador = 4 espacios por efecto  
+        //System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante); 
+       // el "\n" es un salto de linea
+    }
+}
